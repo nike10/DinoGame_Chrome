464ec64f036e9ee30974c8e4811840f1790cb7d8
nike10 <58499539+nike10@users.noreply.github.com>
2020-11-04 13:13:54 +0530

    Add files via upload

---
+++ dinogame.py
@@ -0,0 +1,27 @@
+
+
+import pyautogui
+import time
+import keyboard
+import win32api, win32con
+         
+x=801
+y=245
+#R=83 G=83 B=83 
+
+while 1:
+    if pyautogui.pixel(805,209)==(83,83,83):
+        pyautogui.keyDown('down')
+        time.sleep(0.01)
+        pyautogui.keyUp('down')
+    if pyautogui.pixel(x,y)!=(255,255,255):
+        pyautogui.keyDown('space')
+        time.sleep(0.01)
+        pyautogui.keyUp('space')
+    if keyboard.is_pressed('esc')==True:
+        break
+    
+    
+    
+    
+    
